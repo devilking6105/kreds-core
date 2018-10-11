b409618901515d8f2bf8ade00649c9682feaaa94
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 17:59:50 -0400

    Update intro.h

--- src/qt/intro.h
+++ src/qt/intro.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2011-2016 The Kreds Developers
+// Copyright (c) 2011-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KREDS_QT_INTRO_H
-#define KREDS_QT_INTRO_H
+#ifndef BITCOIN_QT_INTRO_H
+#define BITCOIN_QT_INTRO_H
 
 #include <QDialog>
 #include <QMutex>
@@ -35,13 +35,10 @@ public:
     /**
      * Determine data directory. Let the user choose if the current one doesn't exist.
      *
-     * @returns true if a data directory was selected, false if the user cancelled the selection
-     * dialog.
-     *
      * @note do NOT call global GetDataDir() before calling this function, this
      * will cause the wrong path to be cached.
      */
-    static bool pickDataDirectory();
+    static void pickDataDirectory();
 
     /**
      * Determine default data directory for operating system.
@@ -75,4 +72,4 @@ private:
     friend class FreespaceChecker;
 };
 
-#endif // KREDS_QT_INTRO_H
+#endif // BITCOIN_QT_INTRO_H
