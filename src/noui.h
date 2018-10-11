959f8e01aed75d435c1635873482c1a7d2b28015
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 03:02:36 -0400

    Update noui.h

--- src/noui.h
+++ src/noui.h
@@ -1,10 +1,10 @@
-// Copyright (c) 2013-2014 The Kreds Developers
+// Copyright (c) 2013 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KREDS_NOUI_H
-#define KREDS_NOUI_H
+#ifndef BITCOIN_NOUI_H
+#define BITCOIN_NOUI_H
 
 extern void noui_connect();
 
-#endif // KREDS_NOUI_H
+#endif // BITCOIN_NOUI_H
