4f537a71253543952d07b1c5b3226937459ac9fb
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 15:51:58 -0400

    Update scheduler.h

--- src/scheduler.h
+++ src/scheduler.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2015 The Kreds Developers
+// Copyright (c) 2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KREDS_SCHEDULER_H
-#define KREDS_SCHEDULER_H
+#ifndef BITCOIN_SCHEDULER_H
+#define BITCOIN_SCHEDULER_H
 
 //
 // NOTE:
