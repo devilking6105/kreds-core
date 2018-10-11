58c150d679f6942d3db7d73894fe81583e79c1fd
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 19:24:43 -0400

    Update macnotificationhandler.mm

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.kredsfoundation.Kreds-Qt";
+        return @"org.hth.HTH-Qt";
     } else {
         return [self __bundleIdentifier];
     }
