d28ac345731d6056d93d73a1f21a361264fa30ab
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 17:39:21 -0400

    Update guiutil.h

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2011-2016 The Kreds Developers
+// Copyright (c) 2011-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KREDS_QT_GUIUTIL_H
-#define KREDS_QT_GUIUTIL_H
+#ifndef BITCOIN_QT_GUIUTIL_H
+#define BITCOIN_QT_GUIUTIL_H
 
 #include "amount.h"
 
@@ -14,7 +14,6 @@
 #include <QProgressBar>
 #include <QString>
 #include <QTableView>
-#include <QLabel>
 
 #include <boost/filesystem.hpp>
 
@@ -30,7 +29,7 @@ class QUrl;
 class QWidget;
 QT_END_NAMESPACE
 
-/** Utility functions used by the Kreds Qt UI.
+/** Utility functions used by the HTH Qt UI.
  */
 namespace GUIUtil
 {
@@ -45,10 +44,10 @@ namespace GUIUtil
     void setupAddressWidget(QValidatedLineEdit *widget, QWidget *parent);
     void setupAmountWidget(QLineEdit *widget, QWidget *parent);
 
-    // Parse "kreds:" URI into recipient object, return true on successful parsing
-    bool parseKredsURI(const QUrl &uri, SendCoinsRecipient *out);
-    bool parseKredsURI(QString uri, SendCoinsRecipient *out);
-    QString formatKredsURI(const SendCoinsRecipient &info);
+    // Parse "hth:" URI into recipient object, return true on successful parsing
+    bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out);
+    bool parseBitcoinURI(QString uri, SendCoinsRecipient *out);
+    QString formatBitcoinURI(const SendCoinsRecipient &info);
 
     // Returns true if given address+amount meets "dust" definition
     bool isDust(const QString& address, const CAmount& amount);
@@ -68,15 +67,15 @@ namespace GUIUtil
     /** Return a field of the currently selected entry as a QString. Does nothing if nothing
         is selected.
        @param[in] column  Data column to extract from the model
+       @param[in] role    Data role to extract from the model
        @see  TransactionView::copyLabel, TransactionView::copyAmount, TransactionView::copyAddress
      */
-    QList<QModelIndex> getEntryData(QAbstractItemView *view, int column);
+    QString getEntryData(QAbstractItemView *view, int column, int role);
 
     void setClipboard(const QString& str);
 
     /** Get save filename, mimics QFileDialog::getSaveFileName, except that it appends a default suffix
         when no suffix is provided by the user.
-
       @param[in] parent  Parent window (or 0)
       @param[in] caption Window caption (or empty, for default)
       @param[in] dir     Starting directory (or empty, to default to documents directory)
@@ -89,7 +88,6 @@ namespace GUIUtil
         QString *selectedSuffixOut);
 
     /** Get open filename, convenience wrapper for QFileDialog::getOpenFileName.
-
       @param[in] parent  Parent window (or 0)
       @param[in] caption Window caption (or empty, for default)
       @param[in] dir     Starting directory (or empty, to default to documents directory)
@@ -102,7 +100,6 @@ namespace GUIUtil
         QString *selectedSuffixOut);
 
     /** Get connection type to call object slot in GUI thread with invokeMethod. The call will be blocking.
-
        @returns If called from the GUI thread, return a Qt::DirectConnection.
                 If called from another thread, return a Qt::BlockingQueuedConnection.
     */
@@ -113,14 +110,16 @@ namespace GUIUtil
 
     // Open debug.log
     void openDebugLogfile();
+	
+    // Open hth.conf
+    void openConfigfile();	
 
-	// Browse backup folder
-	void showBackups();
-	
-	void showConf();
-	
-	void showKredsConf();
-	
+    // Open masternode.conf
+    void openMNConfigfile();	
+
+    // Browse backup folder
+    void showBackups();
+
     // Replace invalid default fonts with known good ones
     void SubstituteFonts(const QString& language);
 
@@ -147,7 +146,7 @@ namespace GUIUtil
      * Also makes sure the column widths are never larger than the table's viewport.
      * In Qt, all columns are resizable from the right, but it's not intuitive resizing the last column from the right.
      * Usually our second to last columns behave as if stretched, and when on strech mode, columns aren't resizable
-     * interactively or programmatically.
+     * interactively or programatically.
      *
      * This helper object takes care of this issue.
      *
@@ -157,7 +156,7 @@ namespace GUIUtil
         Q_OBJECT
 
         public:
-            TableViewLastColumnResizingFixer(QTableView* table, int lastColMinimumWidth, int allColsMinimumWidth, QObject *parent);
+            TableViewLastColumnResizingFixer(QTableView* table, int lastColMinimumWidth, int allColsMinimumWidth);
             void stretchColumnWidth(int column);
 
         private:
@@ -184,12 +183,20 @@ namespace GUIUtil
     bool GetStartOnSystemStartup();
     bool SetStartOnSystemStartup(bool fAutoStart);
 
+    /** Modify Qt network specific settings on migration */
+    void migrateQtSettings();
+
     /** Save window size and position */
     void saveWindowGeometry(const QString& strSetting, QWidget *parent);
     /** Restore window size and position */
     void restoreWindowGeometry(const QString& strSetting, const QSize &defaultSizeIn, QWidget *parent);
+
     /** Load global CSS theme */
     QString loadStyleSheet();
+
+    /** Return name of current CSS theme */
+    QString getThemeName();
+    
     /* Convert QString to OS specific boost path through UTF-8 */
     boost::filesystem::path qstringToBoostPath(const QString &path);
 
@@ -208,48 +215,20 @@ namespace GUIUtil
     /* Format a CNodeCombinedStats.nTimeOffset into a user-readable string. */
     QString formatTimeOffset(int64_t nTimeOffset);
 
-    QString formatNiceTimeOffset(qint64 secs);
-
-    class ClickableLabel : public QLabel
-    {
-        Q_OBJECT
-
-    Q_SIGNALS:
-        /** Emitted when the label is clicked. The relative mouse coordinates of the click are
-         * passed to the signal.
-         */
-        void clicked(const QPoint& point);
-    protected:
-        void mouseReleaseEvent(QMouseEvent *event);
-    };
-    
-    class ClickableProgressBar : public QProgressBar
-    {
-        Q_OBJECT
-        
-    Q_SIGNALS:
-        /** Emitted when the progressbar is clicked. The relative mouse coordinates of the click are
-         * passed to the signal.
-         */
-        void clicked(const QPoint& point);
-    protected:
-        void mouseReleaseEvent(QMouseEvent *event);
-    };
-
 #if defined(Q_OS_MAC) && QT_VERSION >= 0x050000
     // workaround for Qt OSX Bug:
     // https://bugreports.qt-project.org/browse/QTBUG-15631
     // QProgressBar uses around 10% CPU even when app is in background
-    class ProgressBar : public ClickableProgressBar
+    class ProgressBar : public QProgressBar
     {
         bool event(QEvent *e) {
             return (e->type() != QEvent::StyleAnimationUpdate) ? QProgressBar::event(e) : false;
         }
     };
 #else
-    typedef ClickableProgressBar ProgressBar;
+    typedef QProgressBar ProgressBar;
 #endif
 
 } // namespace GUIUtil
 
-#endif // KREDS_QT_GUIUTIL_H
+#endif // BITCOIN_QT_GUIUTIL_H
