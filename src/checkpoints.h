4ed14c3ece09c635fb9d491968d0c362441ed27c
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 02:07:17 -0400

    Update checkpoints.h

--- src/checkpoints.h
+++ src/checkpoints.h
@@ -1,11 +1,9 @@
-// Copyright (c) 2009-2017 The Bitcoin Core developers 
-// Copyright (c) 2015-2017 The Dash developers 
-// Copyright (c) 2015-2017 The Kreds developers
+// Copyright (c) 2009-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KREDS_CHECKPOINTS_H
-#define KREDS_CHECKPOINTS_H
+#ifndef BITCOIN_CHECKPOINTS_H
+#define BITCOIN_CHECKPOINTS_H
 
 #include "uint256.h"
 
@@ -21,9 +19,14 @@ struct CCheckpointData;
 namespace Checkpoints
 {
 
+//! Return conservative estimate of total number of blocks, 0 if unknown
+int GetTotalBlocksEstimate(const CCheckpointData& data);
+
 //! Returns last CBlockIndex* in mapBlockIndex that is a checkpoint
 CBlockIndex* GetLastCheckpoint(const CCheckpointData& data);
 
+double GuessVerificationProgress(const CCheckpointData& data, CBlockIndex* pindex, bool fSigchecks = true);
+
 } //namespace Checkpoints
 
-#endif // KREDS_CHECKPOINTS_H
+#endif // BITCOIN_CHECKPOINTS_H
