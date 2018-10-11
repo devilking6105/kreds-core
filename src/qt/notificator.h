d71cf4473e53614ac77f21360e4c5b70e90c09d1
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 19:30:48 -0400

    Update notificator.h

--- src/qt/notificator.h
+++ src/qt/notificator.h
@@ -1,12 +1,12 @@
-// Copyright (c) 2011-2015 The Kreds Developers
+// Copyright (c) 2011-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KREDS_QT_NOTIFICATOR_H
-#define KREDS_QT_NOTIFICATOR_H
+#ifndef BITCOIN_QT_NOTIFICATOR_H
+#define BITCOIN_QT_NOTIFICATOR_H
 
 #if defined(HAVE_CONFIG_H)
-#include "config/kreds-config.h"
+#include "config/hth-config.h"
 #endif
 
 #include <QIcon>
@@ -77,4 +77,4 @@ private:
 #endif
 };
 
-#endif // KREDS_QT_NOTIFICATOR_H
+#endif // BITCOIN_QT_NOTIFICATOR_H
