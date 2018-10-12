8cc0261310e1bc00641c278c09dee45b27e466ea
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 20:41:41 -0400

    Update transactiontablemodel.h

--- src/qt/transactiontablemodel.h
+++ src/qt/transactiontablemodel.h
@@ -1,11 +1,11 @@
-// Copyright (c) 2011-2016 The Kreds Developers
+// Copyright (c) 2011-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KREDS_QT_TRANSACTIONTABLEMODEL_H
-#define KREDS_QT_TRANSACTIONTABLEMODEL_H
+#ifndef BITCOIN_QT_TRANSACTIONTABLEMODEL_H
+#define BITCOIN_QT_TRANSACTIONTABLEMODEL_H
 
-#include "kredsunits.h"
+#include "bitcoinunits.h"
 
 #include <QAbstractTableModel>
 #include <QStringList>
@@ -62,8 +62,6 @@ public:
         TxHashRole,
         /** Transaction data, hex-encoded */
         TxHexRole,
-        /** Whole transaction as plain text */
-        TxPlainTextRole,
         /** Is transaction confirmed? */
         ConfirmedRole,
         /** Formatted amount, without brackets when unconfirmed */
@@ -98,7 +96,7 @@ private:
     QString formatTxDate(const TransactionRecord *wtx) const;
     QString formatTxType(const TransactionRecord *wtx) const;
     QString formatTxToAddress(const TransactionRecord *wtx, bool tooltip) const;
-    QString formatTxAmount(const TransactionRecord *wtx, bool showUnconfirmed=true, KredsUnits::SeparatorStyle separators=KredsUnits::separatorStandard) const;
+    QString formatTxAmount(const TransactionRecord *wtx, bool showUnconfirmed=true, BitcoinUnits::SeparatorStyle separators=BitcoinUnits::separatorStandard) const;
     QString formatTooltip(const TransactionRecord *rec) const;
     QVariant txStatusDecoration(const TransactionRecord *wtx) const;
     QVariant txWatchonlyDecoration(const TransactionRecord *wtx) const;
@@ -117,4 +115,4 @@ public Q_SLOTS:
     friend class TransactionTablePriv;
 };
 
-#endif // KREDS_QT_TRANSACTIONTABLEMODEL_H
+#endif // BITCOIN_QT_TRANSACTIONTABLEMODEL_H
