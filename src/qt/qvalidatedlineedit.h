c9148d499d3fc03ca58f1fd3464e0e99b1f73845
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 20:23:44 -0400

    Update qvalidatedlineedit.h

--- src/qt/qvalidatedlineedit.h
+++ src/qt/qvalidatedlineedit.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2011-2015 The Kreds Developers
+// Copyright (c) 2011-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KREDS_QT_QVALIDATEDLINEEDIT_H
-#define KREDS_QT_QVALIDATEDLINEEDIT_H
+#ifndef BITCOIN_QT_QVALIDATEDLINEEDIT_H
+#define BITCOIN_QT_QVALIDATEDLINEEDIT_H
 
 #include <QLineEdit>
 
@@ -40,4 +40,4 @@ private Q_SLOTS:
     void checkValidity();
 };
 
-#endif // KREDS_QT_QVALIDATEDLINEEDIT_H
+#endif // BITCOIN_QT_QVALIDATEDLINEEDIT_H
