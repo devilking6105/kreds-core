a634cf9011c15b367d7172268a9b97ede3354d95
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 20:26:49 -0400

    Update recentrequeststablemodel.h

--- src/qt/recentrequeststablemodel.h
+++ src/qt/recentrequeststablemodel.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2011-2016 The Kreds Developers
+// Copyright (c) 2011-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KREDS_QT_RECENTREQUESTSTABLEMODEL_H
-#define KREDS_QT_RECENTREQUESTSTABLEMODEL_H
+#ifndef BITCOIN_QT_RECENTREQUESTSTABLEMODEL_H
+#define BITCOIN_QT_RECENTREQUESTSTABLEMODEL_H
 
 #include "walletmodel.h"
 
@@ -27,10 +27,11 @@ public:
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action) {
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
         unsigned int nDate = date.toTime_t();
 
         READWRITE(this->nVersion);
+        nVersion = this->nVersion;
         READWRITE(id);
         READWRITE(nDate);
         READWRITE(recipient);
@@ -52,7 +53,7 @@ private:
     Qt::SortOrder order;
 };
 
-/** Model for list of recently generated payment requests / kreds: URIs.
+/** Model for list of recently generated payment requests / hth: URIs.
  * Part of wallet model.
  */
 class RecentRequestsTableModel: public QAbstractTableModel
@@ -104,4 +105,4 @@ private:
     QString getAmountTitle();
 };
 
-#endif // KREDS_QT_RECENTREQUESTSTABLEMODEL_H
+#endif // BITCOIN_QT_RECENTREQUESTSTABLEMODEL_H
