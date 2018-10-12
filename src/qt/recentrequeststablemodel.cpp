c30684a0367902da19fc9e6984168fd972ac4c57
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-11 20:26:23 -0400

    Update recentrequeststablemodel.cpp

--- src/qt/recentrequeststablemodel.cpp
+++ src/qt/recentrequeststablemodel.cpp
@@ -1,240 +1,201 @@
-// Copyright (c) 2011-2016 The Kreds Developers
+// Copyright (c) 2011-2013 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "recentrequeststablemodel.h"
+#include "receiverequestdialog.h"
+#include "ui_receiverequestdialog.h"
 
-#include "kredsunits.h"
+#include "bitcoinunits.h"
+#include "guiconstants.h"
 #include "guiutil.h"
 #include "optionsmodel.h"
+#include "walletmodel.h"
 
-#include "clientversion.h"
-#include "streams.h"
+#include <QClipboard>
+#include <QDrag>
+#include <QMenu>
+#include <QMimeData>
+#include <QMouseEvent>
+#include <QPixmap>
+#if QT_VERSION < 0x050000
+#include <QUrl>
+#endif
 
-#include <boost/foreach.hpp>
+#if defined(HAVE_CONFIG_H)
+#include "config/hth-config.h" /* for USE_QRCODE */
+#endif
 
-RecentRequestsTableModel::RecentRequestsTableModel(CWallet *wallet, WalletModel *parent) :
-    QAbstractTableModel(parent), walletModel(parent)
+#ifdef USE_QRCODE
+#include <qrencode.h>
+#endif
+
+QRImageWidget::QRImageWidget(QWidget *parent):
+    QLabel(parent), contextMenu(0)
 {
-    Q_UNUSED(wallet);
-    nReceiveRequestsMaxId = 0;
-
-    // Load entries from wallet
-    std::vector<std::string> vReceiveRequests;
-    parent->loadReceiveRequests(vReceiveRequests);
-    BOOST_FOREACH(const std::string& request, vReceiveRequests)
-        addNewRequest(request);
-
-    /* These columns must match the indices in the ColumnIndex enumeration */
-    columns << tr("Date") << tr("Label") << tr("Message") << getAmountTitle();
-
-    connect(walletModel->getOptionsModel(), SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
+    contextMenu = new QMenu();
+    QAction *saveImageAction = new QAction(tr("&Save Image..."), this);
+    connect(saveImageAction, SIGNAL(triggered()), this, SLOT(saveImage()));
+    contextMenu->addAction(saveImageAction);
+    QAction *copyImageAction = new QAction(tr("&Copy Image"), this);
+    connect(copyImageAction, SIGNAL(triggered()), this, SLOT(copyImage()));
+    contextMenu->addAction(copyImageAction);
 }
 
-RecentRequestsTableModel::~RecentRequestsTableModel()
+QImage QRImageWidget::exportImage()
 {
-    /* Intentionally left empty */
+    if(!pixmap())
+        return QImage();
+    return pixmap()->toImage().scaled(EXPORT_IMAGE_SIZE, EXPORT_IMAGE_SIZE);
 }
 
-int RecentRequestsTableModel::rowCount(const QModelIndex &parent) const
+void QRImageWidget::mousePressEvent(QMouseEvent *event)
 {
-    Q_UNUSED(parent);
-
-    return list.length();
-}
-
-int RecentRequestsTableModel::columnCount(const QModelIndex &parent) const
-{
-    Q_UNUSED(parent);
-
-    return columns.length();
-}
-
-QVariant RecentRequestsTableModel::data(const QModelIndex &index, int role) const
-{
-    if(!index.isValid() || index.row() >= list.length())
-        return QVariant();
-
-    const RecentRequestEntry *rec = &list[index.row()];
-
-    if(role == Qt::DisplayRole || role == Qt::EditRole)
+    if(event->button() == Qt::LeftButton && pixmap())
     {
-        switch(index.column())
-        {
-        case Date:
-            return GUIUtil::dateTimeStr(rec->date);
-        case Label:
-            if(rec->recipient.label.isEmpty() && role == Qt::DisplayRole)
-            {
-                return tr("(no label)");
-            }
-            else
-            {
-                return rec->recipient.label;
-            }
-        case Message:
-            if(rec->recipient.message.isEmpty() && role == Qt::DisplayRole)
-            {
-                return tr("(no message)");
-            }
-            else
-            {
-                return rec->recipient.message;
-            }
-        case Amount:
-            if (rec->recipient.amount == 0 && role == Qt::DisplayRole)
-                return tr("(no amount requested)");
-            else if (role == Qt::EditRole)
-                return KredsUnits::format(walletModel->getOptionsModel()->getDisplayUnit(), rec->recipient.amount, false, KredsUnits::separatorNever);
-            else
-                return KredsUnits::format(walletModel->getOptionsModel()->getDisplayUnit(), rec->recipient.amount);
-        }
-    }
-    else if (role == Qt::TextAlignmentRole)
-    {
-        if (index.column() == Amount)
-            return (int)(Qt::AlignRight|Qt::AlignVCenter);
-    }
-    return QVariant();
-}
+        event->accept();
+        QMimeData *mimeData = new QMimeData;
+        mimeData->setImageData(exportImage());
 
-bool RecentRequestsTableModel::setData(const QModelIndex &index, const QVariant &value, int role)
-{
-    return true;
-}
-
-QVariant RecentRequestsTableModel::headerData(int section, Qt::Orientation orientation, int role) const
-{
-    if(orientation == Qt::Horizontal)
-    {
-        if(role == Qt::DisplayRole && section < columns.size())
-        {
-            return columns[section];
-        }
-    }
-    return QVariant();
-}
-
-/** Updates the column title to "Amount (DisplayUnit)" and emits headerDataChanged() signal for table headers to react. */
-void RecentRequestsTableModel::updateAmountColumnTitle()
-{
-    columns[Amount] = getAmountTitle();
-    Q_EMIT headerDataChanged(Qt::Horizontal,Amount,Amount);
-}
-
-/** Gets title for amount column including current display unit if optionsModel reference available. */
-QString RecentRequestsTableModel::getAmountTitle()
-{
-    return (this->walletModel->getOptionsModel() != NULL) ? tr("Requested") + " ("+KredsUnits::name(this->walletModel->getOptionsModel()->getDisplayUnit()) + ")" : "";
-}
-
-QModelIndex RecentRequestsTableModel::index(int row, int column, const QModelIndex &parent) const
-{
-    Q_UNUSED(parent);
-
-    return createIndex(row, column);
-}
-
-bool RecentRequestsTableModel::removeRows(int row, int count, const QModelIndex &parent)
-{
-    Q_UNUSED(parent);
-
-    if(count > 0 && row >= 0 && (row+count) <= list.size())
-    {
-        const RecentRequestEntry *rec;
-        for (int i = 0; i < count; ++i)
-        {
-            rec = &list[row+i];
-            if (!walletModel->saveReceiveRequest(rec->recipient.address.toStdString(), rec->id, ""))
-                return false;
-        }
-
-        beginRemoveRows(parent, row, row + count - 1);
-        list.erase(list.begin() + row, list.begin() + row + count);
-        endRemoveRows();
-        return true;
+        QDrag *drag = new QDrag(this);
+        drag->setMimeData(mimeData);
+        drag->exec();
     } else {
-        return false;
+        QLabel::mousePressEvent(event);
     }
 }
 
-Qt::ItemFlags RecentRequestsTableModel::flags(const QModelIndex &index) const
+void QRImageWidget::saveImage()
 {
-    return Qt::ItemIsSelectable | Qt::ItemIsEnabled;
-}
-
-// called when adding a request from the GUI
-void RecentRequestsTableModel::addNewRequest(const SendCoinsRecipient &recipient)
-{
-    RecentRequestEntry newEntry;
-    newEntry.id = ++nReceiveRequestsMaxId;
-    newEntry.date = QDateTime::currentDateTime();
-    newEntry.recipient = recipient;
-
-    CDataStream ss(SER_DISK, CLIENT_VERSION);
-    ss << newEntry;
-
-    if (!walletModel->saveReceiveRequest(recipient.address.toStdString(), newEntry.id, ss.str()))
+    if(!pixmap())
         return;
-
-    addNewRequest(newEntry);
-}
-
-// called from ctor when loading from wallet
-void RecentRequestsTableModel::addNewRequest(const std::string &recipient)
-{
-    std::vector<char> data(recipient.begin(), recipient.end());
-    CDataStream ss(data, SER_DISK, CLIENT_VERSION);
-
-    RecentRequestEntry entry;
-    ss >> entry;
-
-    if (entry.id == 0) // should not happen
-        return;
-
-    if (entry.id > nReceiveRequestsMaxId)
-        nReceiveRequestsMaxId = entry.id;
-
-    addNewRequest(entry);
-}
-
-// actually add to table in GUI
-void RecentRequestsTableModel::addNewRequest(RecentRequestEntry &recipient)
-{
-    beginInsertRows(QModelIndex(), 0, 0);
-    list.prepend(recipient);
-    endInsertRows();
-}
-
-void RecentRequestsTableModel::sort(int column, Qt::SortOrder order)
-{
-    qSort(list.begin(), list.end(), RecentRequestEntryLessThan(column, order));
-    Q_EMIT dataChanged(index(0, 0, QModelIndex()), index(list.size() - 1, NUMBER_OF_COLUMNS - 1, QModelIndex()));
-}
-
-void RecentRequestsTableModel::updateDisplayUnit()
-{
-    updateAmountColumnTitle();
-}
-
-bool RecentRequestEntryLessThan::operator()(RecentRequestEntry &left, RecentRequestEntry &right) const
-{
-    RecentRequestEntry *pLeft = &left;
-    RecentRequestEntry *pRight = &right;
-    if (order == Qt::DescendingOrder)
-        std::swap(pLeft, pRight);
-
-    switch(column)
+    QString fn = GUIUtil::getSaveFileName(this, tr("Save QR Code"), QString(), tr("PNG Image (*.png)"), NULL);
+    if (!fn.isEmpty())
     {
-    case RecentRequestsTableModel::Date:
-        return pLeft->date.toTime_t() < pRight->date.toTime_t();
-    case RecentRequestsTableModel::Label:
-        return pLeft->recipient.label < pRight->recipient.label;
-    case RecentRequestsTableModel::Message:
-        return pLeft->recipient.message < pRight->recipient.message;
-    case RecentRequestsTableModel::Amount:
-        return pLeft->recipient.amount < pRight->recipient.amount;
-    default:
-        return pLeft->id < pRight->id;
+        exportImage().save(fn);
     }
+}
+
+void QRImageWidget::copyImage()
+{
+    if(!pixmap())
+        return;
+    QApplication::clipboard()->setImage(exportImage());
+}
+
+void QRImageWidget::contextMenuEvent(QContextMenuEvent *event)
+{
+    if(!pixmap())
+        return;
+    contextMenu->exec(event->globalPos());
+}
+
+ReceiveRequestDialog::ReceiveRequestDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ReceiveRequestDialog),
+    model(0)
+{
+    ui->setupUi(this);
+
+#ifndef USE_QRCODE
+    ui->btnSaveAs->setVisible(false);
+    ui->lblQRCode->setVisible(false);
+#endif
+
+    connect(ui->btnSaveAs, SIGNAL(clicked()), ui->lblQRCode, SLOT(saveImage()));
+}
+
+ReceiveRequestDialog::~ReceiveRequestDialog()
+{
+    delete ui;
+}
+
+void ReceiveRequestDialog::setModel(OptionsModel *model)
+{
+    this->model = model;
+
+    if (model)
+        connect(model, SIGNAL(displayUnitChanged(int)), this, SLOT(update()));
+
+    // update the display unit if necessary
+    update();
+}
+
+void ReceiveRequestDialog::setInfo(const SendCoinsRecipient &info)
+{
+    this->info = info;
+    update();
+}
+
+void ReceiveRequestDialog::update()
+{
+    if(!model)
+        return;
+    QString target = info.label;
+    if(target.isEmpty())
+        target = info.address;
+    setWindowTitle(tr("Request payment to %1").arg(target));
+
+    QString uri = GUIUtil::formatBitcoinURI(info);
+    ui->btnSaveAs->setEnabled(false);
+    QString html;
+    html += "<html><font face='verdana, arial, helvetica, sans-serif'>";
+    html += "<b>"+tr("Payment information")+"</b><br>";
+    html += "<b>"+tr("URI")+"</b>: ";
+    html += "<a href=\""+uri+"\">" + GUIUtil::HtmlEscape(uri) + "</a><br>";
+    html += "<b>"+tr("Address")+"</b>: " + GUIUtil::HtmlEscape(info.address) + "<br>";
+    if(info.amount)
+        html += "<b>"+tr("Amount")+"</b>: " + BitcoinUnits::formatHtmlWithUnit(model->getDisplayUnit(), info.amount) + "<br>";
+    if(!info.label.isEmpty())
+        html += "<b>"+tr("Label")+"</b>: " + GUIUtil::HtmlEscape(info.label) + "<br>";
+    if(!info.message.isEmpty())
+        html += "<b>"+tr("Message")+"</b>: " + GUIUtil::HtmlEscape(info.message) + "<br>";
+    if(info.fUseInstantSend)
+        html += "<b>"+tr("InstantSend")+"</b>: Yes<br>";
+    else
+        html += "<b>"+tr("InstantSend")+"</b>: No<br>";
+    ui->outUri->setText(html);
+
+#ifdef USE_QRCODE
+    ui->lblQRCode->setText("");
+    if(!uri.isEmpty())
+    {
+        // limit URI length
+        if (uri.length() > MAX_URI_LENGTH)
+        {
+            ui->lblQRCode->setText(tr("Resulting URI too long, try to reduce the text for label / message."));
+        } else {
+            QRcode *code = QRcode_encodeString(uri.toUtf8().constData(), 0, QR_ECLEVEL_L, QR_MODE_8, 1);
+            if (!code)
+            {
+                ui->lblQRCode->setText(tr("Error encoding URI into QR Code."));
+                return;
+            }
+            QImage myImage = QImage(code->width + 8, code->width + 8, QImage::Format_RGB32);
+            myImage.fill(0xffffff);
+            unsigned char *p = code->data;
+            for (int y = 0; y < code->width; y++)
+            {
+                for (int x = 0; x < code->width; x++)
+                {
+                    myImage.setPixel(x + 4, y + 4, ((*p & 1) ? 0x0 : 0xffffff));
+                    p++;
+                }
+            }
+            QRcode_free(code);
+
+            ui->lblQRCode->setPixmap(QPixmap::fromImage(myImage).scaled(300, 300));
+            ui->btnSaveAs->setEnabled(true);
+        }
+    }
+#endif
+}
+
+void ReceiveRequestDialog::on_btnCopyURI_clicked()
+{
+    GUIUtil::setClipboard(GUIUtil::formatBitcoinURI(info));
+}
+
+void ReceiveRequestDialog::on_btnCopyAddress_clicked()
+{
+    GUIUtil::setClipboard(info.address);
 }
